586cd02a4e3d06f07321364cd34859bc770eaf00
tonmoysaha <tonmoysahaopi@gmail.com>
2019-08-25 00:12:53 +0600

    update customer for rest

--- src/main/java/com/spring/springrestcrud/restcontroller/CustomerRestController.java
+++ src/main/java/com/spring/springrestcrud/restcontroller/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +44,13 @@ public class CustomerRestController {
 		theCustomer.setId(0);
 		customerservice.saveCustomer(theCustomer);
 		return theCustomer;
+		
+	}
+	
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
+		customerservice.saveCustomer(customer);
+		return customer;
 		
 	}
 }
